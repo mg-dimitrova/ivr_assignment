93df261bda4600d30d2df2e4e1f30cb966e12d4e
mdimitrova <maria.gloria.dimitrova@gmail.com>
2020-11-05 11:13:40 +0100

    modify the code to correctly save the images taken by the cameras

--- src/image1.py
+++ src/image1.py
@@ -34,7 +34,7 @@ class image_converter:
       print(e)
     
     # Uncomment if you want to save the image
-    #cv2.imwrite('image_copy.png', cv_image)
+    #cv2.imwrite('image_copy1.png', self.cv_image1)
 
     im1=cv2.imshow('window1', self.cv_image1)
     cv2.waitKey(1)
--- src/image2.py
+++ src/image2.py
@@ -33,7 +33,7 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
     # Uncomment if you want to save the image
-    #cv2.imwrite('image_copy.png', cv_image)
+    #cv2.imwrite('image_copy2.png', self.cv_image2)
     im2=cv2.imshow('window2', self.cv_image2)
     cv2.waitKey(1)
 
